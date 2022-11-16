0997b45955f2fc770372e9382148719a9991c99d
Alessio Vierti <alessio.vierti@gmail.com>
2022-11-16 21:37:30 +0100

    Make cache dynamic with respect to online status - Closes #8

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,15 @@
-const initialCache = [];
+const alwaysCache = [
+  '/logo.png',
+  '/assets/tile-bw.png',
+  '/assets/tile-satellite.png',
+  '/assets/tile-street.png',
+];
+
+const cacheForOffline = [
+  '/index.html',
+  '/assets/index.081ff665.css',
+  '/assets/index.dec81e95.js'
+];
 
 const CACHE_VERSION = 'osm-tiles-v1';
 
@@ -6,7 +17,10 @@ self.addEventListener('install', (event) =>
   event.waitUntil(
     caches
       .open(CACHE_VERSION)
-      .then((cache) => cache.addAll(initialCache))
+      .then((cache) => {
+        cache.addAll(alwaysCache);
+        cache.addAll(cacheForOffline);
+      })
   )
 );
 
@@ -21,8 +35,20 @@ self.addEventListener('fetch', (event) =>
       caches
           .match(event.request)
           .then((response) => {
-            if (response) return response;  // Cache hit
-            else {                          // Cache miss
+            if (response) {                 // Cache hit
+              const {pathname} = new URL(event.request.url);
+              // Serve from cache static assets or cached items (when offline):
+              if (cacheForOffline.includes(pathname) && !navigator.onLine || alwaysCache.includes(pathname)) {
+                return response;
+              } else {
+                // Update cache if online:
+                response = fetch(event.request);
+                if (response.ok) {
+                  caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, new Response(response.body, response)));
+                }
+                return response;
+              }
+            } else {                        // Cache miss
               const {hostname} = new URL(event.request.url);
               const response = fetch(event.request);
               if (cachedHosts.includes(hostname) && response.ok) {
