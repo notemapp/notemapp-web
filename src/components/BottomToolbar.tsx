c2331360b5c1200f00669fd8f96de2567c5545c5
Alessio Vierti <alessio.vierti@gmail.com>
2022-11-19 10:03:31 +0100

    Set draw toolbar button style

--- src/components/BottomToolbar.tsx
+++ src/components/BottomToolbar.tsx
@@ -23,6 +23,11 @@ export function BottomToolbar(props: {
     const activeClass = freeHand ? "bg-blue-500 text-white rounded" : "text-black";
     return baseClass + " " + activeClass;
   }
+  const buttonToolbarClass = (thisDrawType: DrawType) => {
+    const baseClass = "w-12 h-full rounded flex justify-center items-center group relative";
+    const activeClass = DrawType.None !== thisDrawType ? "bg-blue-500 text-white rounded" : "text-black";
+    return baseClass + " " + activeClass;
+  }
 
   const onDrawTypeChange = (type: DrawType) => {
     props.onDrawTypeChange(type);
@@ -44,7 +49,7 @@ export function BottomToolbar(props: {
                     d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"/>
             </svg>
           </button>
-          <button className="w-12 h-full rounded flex justify-center items-center group relative text-black">
+          <button className={buttonToolbarClass(drawType)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                  stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round"
